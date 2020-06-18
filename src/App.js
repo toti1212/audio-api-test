df25dda7874b021be4bfd26eaebcdd4ef7c812d7
Rodrigo Suárez <rodrigosrz12@gmail.com>
2020-06-18 20:52:24 -0300

    Testing audios in differents devices and browsers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import bell from './bell.mp3'
+import logo from './logo.svg';
+
 function App() {
+  function play() {
+    var audio = new Audio(bell)
+    audio.play();
+  }
+  
+  
+  function notifyMe() {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
+  
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      navigator.serviceWorker.ready.then(registration => {
+        registration.showNotification('Vibration Sample', {
+          body: 'Buzz! Buzz!',
+          tag: 'vibration-sample'
+        });
+      });
+    }
+  
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== 'denied') {
+      Notification.requestPermission(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          if ('Notification' in window) {
+            navigator.serviceWorker.ready.then(registration => {
+              registration.showNotification('Vibration Sample', {
+                body: 'Buzz! Buzz!',
+                tag: 'vibration-sample'
+              });
+            });
+          }
+      }
+    })
+  }
+}
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +53,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => {play()}}>Instant</button>
+        <button onClick={() => {setInterval(play, 5000)}}>Delay</button>
       </header>
     </div>
   );
